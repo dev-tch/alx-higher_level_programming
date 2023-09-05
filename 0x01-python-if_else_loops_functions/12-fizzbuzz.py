f1b142d09e47848effc2c7b9894066bd1db0c687
dev-tch <tijeni.chaabeni@gmail.com>
2023-09-05 23:38:01 +0000

    fix 12fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    if (i % 3 == 0 and i % 5 == 0):
-        print("{}".format("fizzbuzz"), end=" ")
-    else:
-        if (i % 3 == 0):
-            print("{}".format("fizz"), end=" ")
-        elif (i % 5 == 0):
-            print("{}".format("buzz"), end=" ")
+def fizzbuzz():
+    for i in range(1, 101):
+        if (i % 3 == 0 and i % 5 == 0):
+            print("{}".format("fizzbuzz"), end=" ")
         else:
-            print("{}".format(i), end=" ")
+            if (i % 3 == 0):
+                print("{}".format("fizz"), end=" ")
+            elif (i % 5 == 0):
+                print("{}".format("buzz"), end=" ")
+            else:
+                print("{}".format(i), end=" ")
