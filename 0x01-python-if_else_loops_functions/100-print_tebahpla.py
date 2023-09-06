065080899c10990e28c98553fa4ba7730d727312
dev-tch <tijeni.chaabeni@gmail.com>
2023-09-06 19:52:52 +0000

    fix task 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(26):
     if (i % 2 == 0):
-        print(chr(ord('z') - i), end="")
+        print("{}".format(chr(ord('z') - i)), end="")
     else:
-        print(chr(ord('Z') - i), end="")
+        print("{}".format(chr(ord('Z') - i)), end="")
