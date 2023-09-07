1ebce52bd5710a3fe465609e55cf5e8feb6203e3
dev-tch <tijeni.chaabeni@gmail.com>
2023-09-07 23:21:05 +0000

    fix 102 rev7

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
+    add, sub = __import__('magic_calculation_102', fromlist=('add', 'sub'))
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
