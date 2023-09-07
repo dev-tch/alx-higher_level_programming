61af68f94b2078f1917810177f7f385bc6de56cb
dev-tch <tijeni.chaabeni@gmail.com>
2023-09-07 18:43:11 +0000

    fix task arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,8 @@ if __name__ == "__main__":
     if (nb_args == 1):
         print(f"1 argument:")
         print(f"1: {argv[1]}")
+    elif (nb_args == 0):
+        print("0 arguments.")
     elif (nb_args > 1):
         print(f"{nb_args} arguments:")
         for i in range(1, nb_args + 1):
