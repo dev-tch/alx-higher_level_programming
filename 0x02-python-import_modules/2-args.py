4788fb25e4dfdaa5559317e446d4414690a99ec3
dev-tch <tijeni.chaabeni@gmail.com>
2023-09-07 18:36:57 +0000

    print args python

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    nb_args = len(argv) - 1
+    if (nb_args == 1):
+        print(f"1 argument:")
+        print(f"1: {argv[1]}")
+    elif (nb_args > 1):
+        print(f"{nb_args} arguments:")
+        for i in range(1, nb_args + 1):
+            print(f"{i}: {argv[i]}")
