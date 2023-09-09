414db7ef5f8df175a2e9a0b514d0f39b8c3a3e3e
dev-tch <tijeni.chaabeni@gmail.com>
2023-09-09 23:41:34 +0000

    detect last element using enumerate

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for list in matrix:
-        for item in list:
-            print("{:d}".format(item), end="")
+        for index, item in enumerate(list):
+            if index == len(list) - 1:
+                print("{:d}".format(item), end="")
+            else:
+                print("{:d} ".format(item), end="")
         print("$")
