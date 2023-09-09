79123438314bf47ecb88266f0f23d0fda8e6d7a7
dev-tch <tijeni.chaabeni@gmail.com>
2023-09-09 14:06:28 +0000

    work with Reverse_func task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=None):
+def print_reversed_list_integer(my_list=[]):
     size = len(my_list)
-    if size > 0:
-        for i in range(size - 1, -1, -1):
-            print("{:d}".format(my_list[i]))
+    mylist.Reverse()
+    for i in range(size):
+        print("{:d}".format(my_list[i]))
