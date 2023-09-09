1cb5120e9e46200638f1e6f07fe27cfe0792619b
dev-tch <tijeni.chaabeni@gmail.com>
2023-09-09 14:00:17 +0000

    fix2 print reverse list task3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
+def print_reversed_list_integer(my_list=None):
     size = len(my_list)
     if size > 0:
         for i in range(size - 1, -1, -1):
