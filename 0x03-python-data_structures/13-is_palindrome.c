fd1b1b378a4bff1ea98f5da23b46f05f0afcd3d7
dev-tch <tijeni.chaabeni@gmail.com>
2023-09-11 23:32:14 +0000

    new version task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,76 +1,41 @@
 #include "lists.h"
-
-/*find the middle of list*/
 /**
-* get_node_at_middle - search middle node
-* @h: list
-* Return: pointer to middle node
-*/
-listint_t *get_node_at_middle(listint_t *h)
-{
-	/*declare two pointers at head*/
-	listint_t *jmpone = h,  *jmptwo = h;
-
-	while (jmpone != NULL && jmptwo != NULL)
-	{
-		jmpone = jmpone->next;
-		jmptwo = jmptwo->next;
-	}
-	return (jmpone);
-}
-
-/**/
-/**
-* reversed_list - reverse the second half of list
-* @h: second half list
-* Return: pointer to the list
-*/
-listint_t *reversed_list(listint_t *h)
-{
-	listint_t *before = NULL;
-	listint_t *now = h;
-	listint_t *after = NULL;
-
-	while (now != NULL)
-	{
-		after = now->next;
-		now->next = before;
-		before = now;
-		now = after;
-	}
-	return (before);
-}
-
-/**
-* is_palindrome -  list can be same if we  read from first or last
+* is_palindrome - check if list is palindrome
 * @head: list
-* Return: (0 or 1)
+* Return: (1 or 0)
 */
 int is_palindrome(listint_t **head)
 {
-	listint_t *mid = NULL;
-	listint_t *halftwo =  NULL;
-	listint_t *halfone = *head;
-	int is_pal = 1;
+	listint_t *jmpone = *head;
+	listint_t *jmptwo = *head;
+	listint_t *old = NULL;
+	listint_t *tmp;
 
 	if (*head == NULL || (*head)->next == NULL)
-	return (1);
-
-	mid = get_node_at_middle(*head);
-	if (mid == NULL)
 		return (1);
-	halftwo =  reversed_list(mid->next);
-	while (halftwo != NULL)
+	while (jmptwo != NULL && jmptwo->next != NULL)
 	{
-		if (halfone->n != halftwo->n)
-		{
-			is_pal = 0;
-			break;
-		}
-		halfone = halfone->next;
-		halftwo = halftwo->next;
+		jmptwo = jmptwo->next->next;
+		tmp = jmpone->next;
+		jmpone->next = old;
+		old = jmpone;
+		jmpone = tmp;
 	}
 
-	mid->next = reversed_list(halfone);
-	return (is_pal);
+	if (jmptwo != NULL)
+	{
+		jmpone = jmpone->next;
+	}
+
+	while (old != NULL && jmpone != NULL)
+	{
+
+		if (old->n != jmpone->n)
+		{
+			return (0);
+		}
+	old = old->next;
+	jmpone = jmpone->next;
+	}
+	return (1);
 }
