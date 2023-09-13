e1dc62bff1dd7fb7382831b601d149d26490639b
dev-tch <tijeni.chaabeni@gmail.com>
2023-09-13 22:38:00 +0000

    fix biggest key value dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,9 +3,11 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     i = 0
-    max = None
-    for val in a_dictionary.values():
-        if (i == 0 or val > max):
-            max = val
+    max_val = None
+    max_key = None
+    for key, val in a_dictionary.items():
+        if (i == 0 or val > max_val):
+            max_val = val
+            max_key = key
         i += 1
-    return (max)
+    return (max_key)
