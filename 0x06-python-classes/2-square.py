e86502e7f54baa7853678abb02ad3ed06af8c56f
dev-tch <tijeni.chaabeni@gmail.com>
2023-09-26 20:04:09 +0000

    raise exception case

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define class Square."""
+
+
+class Square:
+    """implement Square."""
+    def __init__(self, size=0):
+
+        """constructor method.
+
+        Args:
+            size(int): side len
+        """
+        if not isintance(size, int):
+            raise TypeError("size must be integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+            
