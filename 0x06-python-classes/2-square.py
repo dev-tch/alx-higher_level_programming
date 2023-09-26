6b8243bed08510a1e01ddac2091ac7392def5e87
dev-tch <tijeni.chaabeni@gmail.com>
2023-09-26 20:08:32 +0000

    fix raise exception with class task2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,7 +12,7 @@ class Square:
             size(int): side len
         """
         if not isintance(size, int):
-            raise TypeError("size must be integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
