ba670ef24f4e43659055d32c11c46cd773fad7c7
dev-tch <tijeni.chaabeni@gmail.com>
2023-09-26 19:52:56 +0000

    square with field size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,18 +3,13 @@
 
 
 class Square:
-    """implement Square class.
+    """implement Square class."""
 
-    Attributes:
-        size(int): side len
-    """
-    __size
     def __init__(self, size):
 
-        """ constructor method
+        """ constructor method.
 
         Args:
-            self(Squre): current object
             size(int): side len
         """
         self.__size = size
