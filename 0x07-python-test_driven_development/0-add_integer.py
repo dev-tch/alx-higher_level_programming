fd2e635b4cf276e122061689ab73384f128d3386
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-03 19:17:28 +0100

    add operation with  doctest

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """Returns a + b
+    Args:
+        a(int): first integer
+        b(int): second integer
+    Returns:
+        int: the sum of a and b
+    Raises:
+          TypeError: either of argument not int of float
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    # conversion to int
+    int_a = int(a)
+    int_b = int(b)
+
+    return int_a + int_b
