8deb282a07ec4af8087a1294675e5ce456cf78a1
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-07 00:26:27 +0100

    print square with #

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+def print_square(size):
+    """
+    print square with symbol #
+    Args:
+        size: the length of side
+
+    Returns:
+        void
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if size == 0:
+        return
+    for j in range(size):
+        print("#" * size, end="")
+        print("")
