6732f29719302eb59c355c848dc5d20cf513e78f
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-06 23:13:18 +0100

    test cases for matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
+"""
+this module have one function to divide each list item in matrix
+"""
+
+
 def matrix_divided(matrix, div):
+    """ divide each item in matrix by div
+    args:
+        matrix(list of list): input matrix
+        div(int): the value used for division
+    Returns:
+          List: list contains others list
+    """
+
     # check if matrix of int and float
     # init local variables
     is_item_of_type_list = True
     is_all_values_list_of_type_int = True
-    for item in matrix:
-        if not isinstance(item, list):
-            is_item_of_type_list = False
-            break
-        if not all(isinstance(val, (int, float)) for val in item):
-            is_all_values_list_of_type_int = False
-            break
+    is_matrix_of_type_list = True
+    if not isinstance(matrix, list):
+        is_matrix_of_type_list = False
+    else:
+        for item in matrix:
+            if not isinstance(item, list):
+                is_item_of_type_list = False
+                break
+            if not all(isinstance(val, (int, float)) for val in item):
+                is_all_values_list_of_type_int = False
+                break
 
-    if not (is_item_of_type_list and is_all_values_list_of_type_int):
+    if (not is_matrix_of_type_list or matrix == [] or
+            not is_item_of_type_list or not is_all_values_list_of_type_int):
         except1 = "matrix must be a matrix (list of lists) of integers/floats"
         raise TypeError(except1)
     # check if all rows of matrix are  same , if not raise except
