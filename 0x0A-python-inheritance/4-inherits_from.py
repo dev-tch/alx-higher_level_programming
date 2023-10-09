da7c03a32d57cb5491b58223ec87fdacbc461165
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-09 23:46:01 +0100

    check instance of subclass

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+module with one function inherits_from
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    return True if Object instance of subclass
+    Args:
+       obj(Object): python object
+       a_class(class): name of class type
+    returns:
+       True of False
+   """
+    return issubclass(obj, a_class)
