2323c7543ea8ecb1367fa218fb38047405961476
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-10 00:16:52 +0100

    add method area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+module with empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """implement class BaseGeometry"""
+
+    def area(self):
+        """
+        method to calculate area of Geometry
+        Raises:
+            Exception
+        """
+        raise Exception("area() is not implemented")
