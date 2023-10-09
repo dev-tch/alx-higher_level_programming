02e2ff600c97870af3d0adbb6c3dcf731c07abfc
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-10 00:51:09 +0100

    add valid_integer meth + test

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+module with  class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """implement class BaseGeometry"""
+
+    def area(self):
+        """
+        method to calculate area of Geometry
+        Raises:
+            Exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        method to validate  integer
+        args:
+            name(str): name of property
+            value(int): value of property
+        Returns:
+                int
+        Raises:
+            Exception
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        elif value < 0:
+            raise ValueError(f"{name} must be greater than 0")
