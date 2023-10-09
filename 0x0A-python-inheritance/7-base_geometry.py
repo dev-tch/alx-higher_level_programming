5973fda984a49dbcbfeb3a3ae376660f16b8ea79
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-10 00:59:50 +0100

    fix test class geametry task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -28,5 +28,5 @@ class BaseGeometry:
         """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError(f"{name} must be greater than 0")
