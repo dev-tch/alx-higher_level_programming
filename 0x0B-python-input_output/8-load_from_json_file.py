0d3502c4db29ddb09ad95556b144e7c765441bed
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-11 00:47:03 +0100

    add new files_test0

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def load_from_json_file(filename):
     """return  json object from file"""
-    with open(filename) as f:
+    with open(filename, mode="r", encoding="utf-8") as f:
         return json.load(f)
