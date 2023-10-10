28aea6bf596367258f66378655472db06d933ca7
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-11 00:33:55 +0100

    new test task7 new try8

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,14 +5,13 @@ script to save and load
 import sys
 
 
-if __name__ == "__main__":
-    save_json = __import__('5-save_to_json_file').save_to_json_file
-    load_json = __import__('6-load_from_json_file').load_from_json_file
-    file = "add_item.json"
-    try:
-        new = load_json(file)
-    except (ValueError, FileNotFoundError):
-        new = []
-    for args in sys.argv[1:]:
-        new.append(args)
-    save_json(new, file)
+save_json = __import__('5-save_to_json_file').save_to_json_file
+load_json = __import__('6-load_from_json_file').load_from_json_file
+file = "add_item.json"
+try:
+    new = load_json(file)
+except (ValueError, FileNotFoundError):
+    new = []
+for args in sys.argv[1:]:
+    new.append(args)
+save_json(new, file)
