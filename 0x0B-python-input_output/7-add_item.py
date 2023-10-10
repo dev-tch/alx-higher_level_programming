4c2684b55ccf4ff8c87139df03556e99ed3adfda
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-11 00:23:28 +0100

    new test task7 new try2

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,32 +1,17 @@
 #!/usr/bin/python3
-""" add arguments to file"""
-from sys import argv
-import json
+"""
+script to save and load
+"""
+import sys
 
+save_json = __import__('7-save_to_json_file').save_to_json_file
+load_json = __import__('8-load_from_json_file').load_from_json_file
 
-def load_from_json_file(file):
-    """return  json object from file"""
-    with open(file) as f:
-        return json.load(f)
-
-
-def save_to_json_file(my_obj, file):
-    """save object with format json into file"""
-    with open(file, 'w') as f:
-        json.dump(my_obj, f)
-
-
-def main():
-    filename = "add_item.json"
-    try:
-        content = load_from_json_file(filename)
-    except FileNotFoundError:
-        content = []
-
-    for i in range(1, len(argv)):
-        content.append(argv[i])
-    save_to_json_file(content, filename)
-
-
-if __name__ == "__main__":
-    main()
+file = "add_item.json"
+try:
+    new = load_json(file)
+except (ValueError, FileNotFoundError):
+    new = []
+for args in sys.argv[1:]:
+    new.append(args)
+save_json(new, file)
