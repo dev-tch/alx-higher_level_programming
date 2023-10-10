6edcba5a2dc12c865640422e68af75de43d37823
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-11 00:10:56 +0100

    new test task7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
-"""access commandline arguments"""
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-"""create object from JSON file"""
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-"""writes an object to text file, using JSON representation"""
+""" add arguments to file"""
 
+
+from sys import argv
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 filename = "add_item.json"
 try:
     content = load_from_json_file(filename)
