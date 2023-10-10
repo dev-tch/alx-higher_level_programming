3639a9626510878aacb355345515f3b53f122b6c
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-11 00:15:45 +0100

    new test task7 new try

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,32 @@
 #!/usr/bin/python3
 """ add arguments to file"""
-
-
 from sys import argv
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-filename = "add_item.json"
-try:
-    content = load_from_json_file(filename)
-except FileNotFoundError:
-    content = []
+import json
 
-for i in range(1, len(argv)):
-    content.append(argv[i])
-save_to_json_file(content, filename)
+
+def load_from_json_file(file):
+    """return  json object from file"""
+    with open(file) as f:
+        return json.load(f)
+
+
+def save_to_json_file(my_obj, file):
+    """save object with format json into file"""
+    with open(file, 'w') as f:
+        json.dump(my_obj, f)
+
+
+def main():
+    filename = "add_item.json"
+    try:
+        content = load_from_json_file(filename)
+    except FileNotFoundError:
+        content = []
+
+    for i in range(1, len(argv)):
+        content.append(argv[i])
+    save_to_json_file(content, filename)
+
+
+if __name__ == "__main__":
+    main()
