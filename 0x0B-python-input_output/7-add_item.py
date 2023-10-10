7b84c9e225372d7d1cb6584d0cc0ea5e56b09db9
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-11 00:31:47 +0100

    new test task7 new try7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,10 +6,13 @@ import sys
 
 
 if __name__ == "__main__":
-    save_json = __import__('7-save_to_json_file').save_to_json_file
-    load_json = __import__('8-load_from_json_file').load_from_json_file
+    save_json = __import__('5-save_to_json_file').save_to_json_file
+    load_json = __import__('6-load_from_json_file').load_from_json_file
     file = "add_item.json"
-    new = load_json(file)
+    try:
+        new = load_json(file)
+    except (ValueError, FileNotFoundError):
+        new = []
     for args in sys.argv[1:]:
         new.append(args)
     save_json(new, file)
