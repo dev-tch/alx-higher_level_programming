9f33fb7dc5f6baf64f3cad5cdb3f950c06dbb4f4
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-10 23:37:50 +0100

    new test task7-try11

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """ add arguments to file"""
 from sys import argv
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+from os.path import exists
 
 
 def main():
-    try:
-        my_list = load_from_json_file("add_item.json")
-    except FileNotFoundError:
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = \
+        __import__("6-load_from_json_file").load_from_json_file
+    filename = "add_item.json"
+    if exists(filename):
+        my_list = load_from_json_file(filename)
+    else:
         my_list = []
 
     # Add command-line arguments to the list
