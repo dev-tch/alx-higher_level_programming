bf8c99898de1338a827b283e941c628f01e06e0d
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-10 23:32:57 +0100

    new test task7-try10

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
-import sys
+""" add arguments to file"""
+from sys import argv
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+def main():
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        my_list = []
+
+    # Add command-line arguments to the list
+    my_list.extend(argv[1:])
+
+    # Save the updated list to "add_item.json"
+    save_to_json_file(my_list, "add_item.json")
+
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-    items = load_from_json_file("add_item.json")
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+    main()
