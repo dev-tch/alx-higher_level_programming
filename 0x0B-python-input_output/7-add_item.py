a43865e590d9372fbca3671483b0b9251039f9f5
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-11 00:07:54 +0100

    new test task7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-""" add arguments to file format json."""
 from sys import argv
-from os.path import exists
+"""access commandline arguments"""
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+"""create object from JSON file"""
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+"""writes an object to text file, using JSON representation"""
 
+filename = "add_item.json"
+try:
+    content = load_from_json_file(filename)
+except FileNotFoundError:
+    content = []
 
-def main():
-    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-    load_from_json_file = \
-        __import__("6-load_from_json_file").load_from_json_file
-    filename = "add_item.json"
-    if exists(filename):
-        my_list = load_from_json_file(filename)
-    else:
-        my_list = []
-
-    if isinstance(my_list, list):
-        # Add command-line arguments to the list
-        my_list.extend(argv[1:])
-        # Save the updated list to "add_item.json"
-        save_to_json_file(my_list, "add_item.json")
-
-
-if __name__ == "__main__":
-    main()
+for i in range(1, len(argv)):
+    content.append(argv[i])
+save_to_json_file(content, filename)
