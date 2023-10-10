7501d81829f506d29f2c388cd3c638fdb4da718d
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-10 21:27:54 +0100

    save object to format json into file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module with one function save_to_json_file
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """save object with format json into file"""
+    with open(filename, 'w', encoding="UTF-8") as f:
+        json.dump(my_obj, f)
