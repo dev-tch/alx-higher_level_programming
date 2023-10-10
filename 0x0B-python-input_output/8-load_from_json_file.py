8c0dab82e880f875d9cf41dbe6bea7771e2ce944
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-11 00:38:12 +0100

    add new files

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module with one function load_from_json_file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """return  json object from file"""
+    with open(filename) as f:
+        return json.load(f)
