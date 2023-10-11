ee39bee663bc1a435d32e547d19ee67ffd22c408
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-11 21:52:19 +0100

    task8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/python3
+"""module with one function"""
+
+
+def class_to_json(obj):
+    """returns the dictionary
+    description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object
+    """
+    return obj.__dict__
