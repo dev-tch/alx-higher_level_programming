bf3ecee444fe2949954f22810525c5a5d7a44e2f
dev-tch <tijeni.chaabeni@gmail.com>
2023-11-15 00:28:29 +0100

    fix task100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,6 +3,12 @@
 -- Database hbtn_0c_0
 -- Table first_table
 -- Field name in first_table
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+USE DATABASE hbtn_0c_0;
+
+CREATE table IF NOT EXISTS first_table(`name` VARCHAR(255));
+
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
