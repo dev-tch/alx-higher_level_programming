67f3f013802e55e9421e10488529b8a4e658e7c7
dev-tch <tijeni.chaabeni@gmail.com>
2023-11-15 00:38:31 +0100

    new fix task 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,15 +1,10 @@
--- Write a script that converts hbtn_0c_0 database to UTF8 
+-- Write a script that converts htbn_0c_0 database to UTF8 
 -- You need to convert all of the following to UTF8:
--- Database hbtn_0c_0
+-- Database htbn_0c_0
 -- Table first_table
 -- Field name in first_table
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
-USE hbtn_0c_0;
-
-CREATE TABLE IF NOT EXISTS first_table(`name` VARCHAR(255));
-
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
