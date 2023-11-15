ecd8224962c110c8d0bbe80ef1d396279d47d740
dev-tch <tijeni.chaabeni@gmail.com>
2023-11-16 00:26:44 +0100

    REV5 TASK9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -4,9 +4,9 @@
 -- You can use only one SELECT statement
 --- The database name will be passed as an argument of the mysql command
 
-USE hbtn_0d_usa;
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
 
-SELECT cities.id, cities.name, states.name 
-FROM cities
-INNER JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
