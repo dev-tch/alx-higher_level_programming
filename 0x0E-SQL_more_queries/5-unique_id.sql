67cef92b4efa844808b261cef62937d5c9ba68f0
dev-tch <tijeni.chaabeni@gmail.com>
2023-11-15 22:01:51 +0100

    fix task5

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,6 @@
 -- Write a script that creates the table unique_id on your MySQL server
 -- If the table unique_id already exists, your script should not fail
-CREATE TABLE IF NOT EXISTS unique_id(
-	id INT DEFAULT 1,
-	UNIQUE(ID),
-	name VARCHER(256)
-	);
+CREATE TABLE IF NOT EXISTS `unique_id` (
+    `id`   INT          DEFAULT 1 UNIQUE,
+    `name` VARCHAR(256)
+);
