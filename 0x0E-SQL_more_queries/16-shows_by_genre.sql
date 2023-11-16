f954c80bc1e7809a617cd428c3bac36dbc48ddce
dev-tch <tijeni.chaabeni@gmail.com>
2023-11-16 02:30:32 +0100

    fix task16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,10 @@
--- Write a script that lists all shows, and all genres linked to that show
-SELECT tv_shows.title, tv_genres.name
-FROM tv_shows
-LEFT JOIN tv_show_genres 
-ON tv_shows.id =  tv_show_genres.show_id
-LEFT JOIN tv_genres 
-ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title , tv_genres.name;
+-- Each record should display: tv_genres.name - rating sum
+-- Results must be sorted in descending order by their rating
+
+SELECT tv_genres.name, SUM(tv_shows_rate.rating) AS rating_sum
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_shows_rate ON tv_shows.id = tv_shows_rate.show_id
+GROUP BY tv_genres.name
+ORDER BY rating_sum DESC;
