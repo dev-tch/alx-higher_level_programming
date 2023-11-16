8af7cadc71e1d56cb8e4fc3e8007b3bf8b298d9f
dev-tch <tijeni.chaabeni@gmail.com>
2023-11-16 02:37:08 +0100

    fix task16

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,10 +1,10 @@
 -- lists all genres in the database
 -- Results must be sorted in descending order by their rating
 
-SELECT tv_genres.name, SUM(tv_shows_rate.rating) AS rating_sum
-FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-LEFT JOIN tv_shows_rate ON tv_shows.id = tv_shows_rate.show_id
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS "rating" FROM tv_shows
+ JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+ JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
-ORDER BY rating_sum DESC;
+ORDER BY SUM(tv_show_ratings.rate) DESC
+;
