ced8bf1f9f6ac31c56197994e61f77eece2ebec8
dev-tch <tijeni.chaabeni@gmail.com>
2023-11-16 01:45:48 +0100

    new fix task13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,13 +1,13 @@
 -- Each record should display: <TV Show genre> - <Number of shows linked to this genre>
 
 SELECT
-    tv_genres.genre AS genre,
+    tv_genres.name AS genre,
     COUNT(*) AS number_of_shows
 FROM
     tv_genres
 JOIN
     tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY
-    tv_genres.genre
+    tv_genres.name
 ORDER BY
     number_of_shows DESC;
