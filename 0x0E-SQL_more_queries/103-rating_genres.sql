d43db83ac4823064eb39d83cdac4f94f5525bad0
dev-tch <tijeni.chaabeni@gmail.com>
2023-11-16 02:32:10 +0100

    add task 103

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,9 +1,10 @@
 -- lists all genres in the database
 -- Results must be sorted in descending order by their rating
-SELECT g.name AS genre,
-       COUNT(*) AS number_of_shows
-  FROM tv_genres AS g
-       INNER JOIN tv_show_genres AS t
-       ON g.id = t.genre_id
- GROUP BY g.name
- ORDER BY number_of_shows DESC;
+
+SELECT tv_genres.name, SUM(tv_shows_rate.rating) AS rating_sum
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_shows_rate ON tv_shows.id = tv_shows_rate.show_id
+GROUP BY tv_genres.name
+ORDER BY rating_sum DESC;
