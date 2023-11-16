94a0b556e7243203b62fd698bd7b555295d6945e
dev-tch <tijeni.chaabeni@gmail.com>
2023-11-16 01:42:20 +0100

    review task13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,8 @@
- -- Each record should display: <TV Show genre> - <Number of shows linked to this genre>
+-- Each record should display: <TV Show genre> - <Number of shows linked to this genre>
+
 SELECT
     tv_genres.genre AS genre,
-    COUNT(tv_show_genres.show_id) AS number_of_shows
+    COUNT(*) AS number_of_shows
 FROM
     tv_genres
 JOIN
