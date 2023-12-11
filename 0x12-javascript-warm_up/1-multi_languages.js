4f371b028c91bdd38cc116286429b40169b81f99
dev-tch <tijeni.chaabeni@gmail.com>
2023-12-11 19:56:46 +0100

    fix script1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let str1 = 'C is fun';
-let str2 = 'Python is cool';
-let str3 = 'JavaScript is amazing';
+const str1 = 'C is fun';
+const str2 = 'Python is cool';
+const str3 = 'JavaScript is amazing';
 console.log(str1);
 console.log(str2);
 console.log(str3);
