3332e1d0f360b98ac7bfdd0240e2c13d6d1b5205
dev-tch <tijeni.chaabeni@gmail.com>
2023-12-17 13:51:02 -0800

    insert new object

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" class state select some object state data"""
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # create new object
+    new_st = State(name="Louisiana")
+    session.add(new_st)
+    session.commit()
+    session.close()
