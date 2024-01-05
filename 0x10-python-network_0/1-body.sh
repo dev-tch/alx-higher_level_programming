144cc6a3dad9a8f128f7f21109ba983b9206d43a
dev-tch <tijeni.chaabeni@gmail.com>
2024-01-05 12:55:33 -0800

    fix disp body 200

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # display only body of status code 200
-[ "$(curl -s -w "%{http_code}" "$1" | tail -c 3)" -eq 200 ] && curl -s "$1"
+[ "$(curl -sL -w "%{http_code}" "$1" | tail -c 3)" -eq 200 ] && curl -sL "$1"
