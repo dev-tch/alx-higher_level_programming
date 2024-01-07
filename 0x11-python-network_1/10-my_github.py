837901aaac98035dc6be8bf4f55ad333e9281bd4
dev-tch <tijeni.chaabeni@gmail.com>
2024-01-07 12:44:15 -0800

    authentication user github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,16 +5,14 @@ and uses the GitHub API to display your id
 """
 from sys import argv
 from requests import get
-
+from requests.auth import HTTPBasicAuth
 if __name__ == "__main__":
     if len(argv) >= 3:
         user = argv[1]
-        url = "https://api.github.com/users/{user}".format(user=user)
         token = argv[2]
-        headers = {'Authorization': f'Basic {user}:{token}',
-                   'Accept': 'application/vnd.github.v3+json'
-                   }
-        res = get(url, headers=headers)
+        url = "https://api.github.com/user"
+        auth = HTTPBasicAuth(user, token)
+        res = get(url, auth=auth)
         if res.status_code == 200:
             data = res.json()
             print(data.get('id'))
