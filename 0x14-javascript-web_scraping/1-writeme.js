492d1a21ce63226382d111b6fdec4a960cfbf859
dev-tch <tijeni.chaabeni@gmail.com>
2024-01-30 14:24:30 -0800

    fix write to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 const fs = require('node:fs');
 const filePath = process.argv[2];
 const data = process.argv[3];
-fs.writeFile(filePath, data, (err) => {
+fs.writeFile(filePath, data, err => {
   if (err) {
     console.error(err);
   }
