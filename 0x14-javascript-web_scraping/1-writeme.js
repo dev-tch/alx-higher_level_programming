4845b8b12544741d07166c4a21d39e5f9654826c
dev-tch <tijeni.chaabeni@gmail.com>
2024-01-30 14:36:42 -0800

    fix write to file version02

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,6 +4,6 @@ const filePath = process.argv[2];
 const data = process.argv[3];
 fs.writeFile(filePath, data, 'utf-8', err => {
   if (err) {
-    console.error(err);
+    console.log(err);
   }
 });
