fb461024655a2017ba57ad50d3e406aa42273cec
dev-tch <tijeni.chaabeni@gmail.com>
2024-01-30 14:39:05 -0800

    write to file version03

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const fs = require('node:fs');
+const fs = require('fs');
 const filePath = process.argv[2];
 const data = process.argv[3];
 fs.writeFile(filePath, data, 'utf-8', err => {
